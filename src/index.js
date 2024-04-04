9827de5e0e3561b3d3b7bcaffdf62ce5274a9831
Ready-Bridge <hanjungyo5533@gmail.com>
2024-04-04 14:30:09 +0900

    [FIX] mongodb 삭제

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const mongodb = require("mongodb").require("./helpers/exception.helper")(); // handle async error
 
 const app = express();
 app.use(express.json());
