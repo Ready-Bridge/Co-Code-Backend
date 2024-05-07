3e3cf63b612e94197f56020fec3c7dcde0a73f67
이정욱 <65989344+ukly@users.noreply.github.com>
2024-05-07 16:59:08 +0900

    [FIX] User schema profile 관련 속성 추가

--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -1,27 +1,41 @@
-const mongoose = require("mongoose");
+  const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    maxlegth: 15,
-    unique: true,
-  },
-  password: {
-    type: String,
-    maxlength: 100, // bcrypt로 암호화이슈 때문에 늘려놓음
-  },
-  nickname: {
-    type: String,
-    maxlength: 45,
-    unique: true,
-  },
-  email: {
-    type: String,
-    maxlength: 45,
-    unique: true,
-  },
-});
+  const userSchema = new mongoose.Schema({
+    userId: {
+      type: String,
+      maxlegth: 15,
+      unique: true,
+    },
+    password: {
+      type: String,
+      maxlength: 100, // bcrypt로 암호화이슈 때문에 늘려놓음
+    },
+    nickname: {
+      type: String,
+      maxlength: 45,
+      unique: true,
+    },
+    email: {
+      type: String,
+      maxlength: 45,
+      unique: true,
+    },
+    acquiredProfile: {
+      type: [Number],
+    },
+    profileImage: {
+      type: Number,
+      default: 0,
+    },
+    acquiredBackground: {
+      type: [Number],
+    },
+    backgroundImage: {
+      type: Number,
+      default: 0,
+    },
+  });
 
-const userModel = mongoose.model("User", userSchema);
+  const userModel = mongoose.model("User", userSchema);
 
-module.exports = { userModel };
+  module.exports = { userModel };
