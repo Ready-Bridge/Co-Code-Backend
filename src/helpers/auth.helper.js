4027b3e2b277d87e7e2aeb51d5751784ddeb5097
HanJunGyo <127172335+Ready-Bridge@users.noreply.github.com>
2024-05-10 16:10:35 +0900

    [CHORE] 배포 에러처리 테스트로 인한 수정
    console.log 추가..

--- src/helpers/auth.helper.js
+++ src/helpers/auth.helper.js
@@ -5,12 +5,14 @@ const { JWT_SECRET } = process.env;
 
 const middleware = (req, res, next) => {
   let token = req.headers["authorization"];
+  console.log(token);
   if (!token) {
     return res.status(401).json({
       message: "Unauthorized",
     });
   }
   token = token.split(" ")[1] ?? "";
+  console.log(token);
   jwt.verify(token, JWT_SECRET, (err, decoded) => {
     if (err || !decoded.userId || !decoded.nickname || !decoded.email) {
       return res.status(401).json({
