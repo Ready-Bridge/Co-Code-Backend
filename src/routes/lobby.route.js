34bf80fd70157a0528f17df63daf779a7ac261a7
이정욱 <65989344+ukly@users.noreply.github.com>
2024-05-14 15:42:51 +0900

    [FIX] profile middle ware 수정

--- src/routes/lobby.route.js
+++ src/routes/lobby.route.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const lobbyController = require("../controllers/lobby.controller");
-const { middleware } = require("../helpers/auth.helper");
 
-router.get("/profile", middleware, lobbyController.profile);
+router.get("/profile", lobbyController.profile);
 router.post("/qa", lobbyController.lobbyPing);
 router.get("/qa", lobbyController.lobbyPing);
 router.get("/search", lobbyController.lobbyPing);
--- src/services/lobby.service.js
+++ src/services/lobby.service.js
@@ -15,14 +15,14 @@ const profile = async ( userId ) => {
       return new HttpResponse(400, "USER_NOT_EXISTS");
     }
 
-    const { nickname, profile, background, item } = user
+    const { nickname, profile, background, item } = user;
 
     return new HttpResponse(200, {
       nickname: nickname,
       profile: profile,
       background: background,
       item: item,
-    })
+    });
   } catch (err) {
     throw err;
   }
