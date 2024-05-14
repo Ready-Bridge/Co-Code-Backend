d02b1579127774b40b3aebc1fa391ecba1c33cc6
이정욱 <65989344+ukly@users.noreply.github.com>
2024-05-14 16:07:44 +0900

    [FIX] profile 비동기 오류 수정

--- src/services/lobby.service.js
+++ src/services/lobby.service.js
@@ -9,18 +9,15 @@ const profile = async ( userId ) => {
   try{
     const user = await userModel.findOne({ userId: userId });
 
-    console.log(user);
+    const { nickname, profile, background, item } = user;
 
-    return new HttpResponse(200, user);
+    return new HttpResponse(200, {
+      nickname: nickname,
+      profile: profile,
+      background: background,
+      item: item,
+    });
 
-    // const { nickname, profile, background, item } = user;
-
-    // return new HttpResponse(200, {
-    //   nickname: nickname,
-    //   profile: profile,
-    //   background: background,
-    //   item: item,
-    // });
   } catch (err) {
     console.log(err);
     throw err;
