810ab3dbf4f920adca9eb984fc43408b4824fe8b
이정욱 <65989344+ukly@users.noreply.github.com>
2024-05-14 15:37:47 +0900

    [FIX] profile 에러 코드2

--- src/services/lobby.service.js
+++ src/services/lobby.service.js
@@ -6,20 +6,27 @@ const lobbyPing = () => {
 };
 
 const profile = async ( userId ) => {
-  const user = await userModel.findOne({ userId: userId });
+  try{
+    const user = await userModel.findOne({ userId: userId });
 
-  if (!user) {
-    return new HttpResponse(400, "USER_NOT_EXISTS");
+    console.log(user);
+
+    if (!user) {
+      return new HttpResponse(400, "USER_NOT_EXISTS");
+    }
+
+    const { nickname, profile, background, item } = user
+
+    return new HttpResponse(200, {
+      nickname: nickname,
+      profile: profile,
+      background: background,
+      item: item,
+    })
+  } catch (err) {
+    throw err;
   }
 
-  const { nickname, profile, background, item } = user
-
-  return new HttpResponse(200, {
-    nickname: nickname,
-    profile: profile,
-    background: background,
-    item: item,
-  })
 };
 
 module.exports = {
